8f0434fe4c690872744b0d873c570923b1bb5187
hala277 <hala.almasharfeh@gmail.com>
2021-09-13 16:55:05 +0300

    solve class-02

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import Navbar from 'react-bootstrap/Navbar'
+
 
 class Header extends React.Component {
-    render() {
+  render() {
     return (
-        <>
-      <h1>Horned Beasts </h1>
+      <>
+        <Navbar bg="primary" variant="dark">
+          <Navbar.Brand href="#home"> <h1>Horned Beasts </h1></Navbar.Brand>
+        </Navbar>
+
       </>
     );
   }
-  }
+}
 
-  export default Header;
-  
+export default Header;
+
