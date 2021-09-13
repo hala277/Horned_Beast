6d6b4a73551068151f7ff888abaebb0a2414681a
hala277 <hala.almasharfeh@gmail.com>
2021-09-13 14:06:38 +0300

    solve lab01

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-// import logo from './logo.svg';
+
 import './App.css';
 import React from 'react';
 import Header from './components/Header'
-// import Main from './components/Main'
+import Main from './components/Main'
 import Footer from './components/Footer'
-import Add from './components/Add-Img'
+
 
 class App extends React.Component {
   render() {
     return (
       <>
         <Header/>
-
-         <Add/>
+       <Main/>
         <Footer/>
       </>
     )
---
+++ src/components/HornedBeast.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class HornedBeast extends React.Component {
+    render() {
+    return (
+        <div>
+    
+        <h2>{this.props.title}</h2>
+        <img  src = {this.props.image_url} alt = {this.props.keyword} title = {this.props.horns}/>
+        <p>{this.props.description}</p>
+    
+      </div>
+    );
+  }
+  }
+
+  export default HornedBeast;
